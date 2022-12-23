cef8a31b2fb28e7bd14f495beca729a4057357e7
TexA2 <quarcz97@mail.ru>
2022-12-23 21:29:21 +0700

    Исправлен баг и идет снег с самого верха

--- snow.c
+++ snow.c
@@ -15,28 +15,22 @@ int main()
 {
     srand(time(NULL));
     char a[I][J];
-   
     
     system("clear");
     newsnow(a); 
     linerand(a); 
-   
-
-   for (int i = 0 ; i < 2000; i++){
-        snow_move(a);
-        linerand(a);
-   }
+    printfield(a);
+    sleep(1);
 
     do
     {
-        system("clear");
         snow_move(a);
         linerand(a);
         printfield(a);
         sleep(1);
+        system("clear");
     } while (1);
     
-
     return 0;
 }
 
@@ -57,29 +51,19 @@ void printfield (char (*s)[J]){
 }
 
 void linerand (char (*s)[J]){
-
     for (int i = 0; i < I; i++)
          s[i][0] = rand()%(100-98);
-    
     for (int i = 0; i < I; i++){
         if (s[i][0] == 1) s[i][0] = '*';
         if (s[i][0] == 0) s[i][0] = ' ';
-    }
-        
+    }   
 }
 
 void snow_move (char (*s)[J])
 {
-    for (int j = J-1; j >= 0 ; j--)
+    for (int j = J-2; j >= 0 ; j--)
         for (int i = 0; i < I; i++)
-
-   
-       s[i][j+1] = s[i][j];
-       /*Проблема почему он не переносит всю строку вниз а только
-       первый элемент s[0][0] и когда она пройдет полный путь
-       s[0][J] только следующий элемент пойдет спускатся в низ.
-       */
-    
+            s[i][j+1] = s[i][j];
 }
 
 
